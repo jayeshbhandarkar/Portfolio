b4de94714b185b6be28d30e2a5c368c583326bfd
Jayesh Bhandarkar <bhandarkarjayesh721@gmail.com>
2024-02-05 16:03:46 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,7 +70,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["IT Engineer ", "Web Developer ", "Data Science Enthusiast ", "Software Developer ", "Freelancer ","Designer "],
+    strings: ["IT Engineer ", "Web Developer ", "Data Science Enthusiast ", "Software Developer ", "Web Designer "],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
